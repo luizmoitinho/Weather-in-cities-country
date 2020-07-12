7b679ff6a7803dd1ca451b8e4a4dbe74193dbe23
=luizmoitinho <luizcarlos_costam@hotmail.com>
2020-07-12 00:41:19 -0300

    #Feature - creat  input search and logo

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
   date_default_timezone_set('America/Bahia');
-  $url = "http://localhost/Pessoal/challenge-accepted/API/routes/index.php/weathers";
-  echo $url;
-  $ch = curl_init($url);
-  $json = json_decode(curl_exec($ch));
-  var_dump($json);  
+  //$url = "http://localhost/Pessoal/challenge-accepted/API/routes/index.php/weathers";
+  //echo $url;
+  //$ch = curl_init($url);
+ // $json = json_decode(curl_exec($ch));
+ // var_dump($json);  
 
 ?>
 <!DOCTYPE html>
@@ -20,19 +20,26 @@
 
   </head>
   <body class="bg-background">
+      <div class="container-fluid header center-horizontal center-vertical bg-blue mb-md-5">
+           <img class="logo-clima-tempo img-fluid" src="./images/logo-white.png">
+      </div>
 
-      <div class="container">
-          <form class="form-weather">
-            <h1 class="mb-4">Clima Tempo</h1>
-            <div class="input-group col-md-6">
+      <div class="container center-horizontal p-0">
+          <form class="form-weather p-0">
+            <div class="input-group col-lg-6 col-md-8 col-12 p-0 m-0">
               <input type="text" class="form-control" placeholder="Pesquise por Cidade ou Estado">
               <div class="input-group-append">
                 <button class="btn btn-primary" type="button" id="button-addon2">Buscar</button>
               </div>
             </div>
-            <p class="mt-3 mb-3 text-muted">© 2020</p>
+            
           <form>
+<!-- s -->
+
+          <p class="mt-3 mb-3 text-muted">© 2020</p>
       </div>
+
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
